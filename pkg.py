f6639ddb97ab07f74bfe8374e00ec30a562628a4
Hatoris <flotianxbernard@gmail.com>
2017-05-18 07:07:07 -0400

    add unit registry percent

--- pkg.py
+++ pkg.py
@@ -1,9 +1,11 @@
+from functools import wraps
+from pint.converters import ScaleConverter
+from pint.unit import UnitDefinition
 import pint
 ureg = pint.UnitRegistry()
 Q_ = ureg.Quantity
 
 #define bmi
 ureg.define('bmi = kg/m**2')
-ureg.define('percent = pct = %')
-
-from functools import wraps
+#define percent, pct
+ureg.define(UnitDefinition('percent', 'pct', (), ScaleConverter(1 / 100.0)))

b8b3769f85fc7ba7ad26b863ce73a5c1da353dfb
Hatoris <flotianxbernard@gmail.com>
2017-05-23 09:40:56 -0400

    add cli folder

---
+++ cli/clicalculpharma.py
@@ -0,0 +1,24 @@
+"""cli application to perform calcul
+
+Usage: 
+    calculPharma.py <calculus> 
+
+Options:
+    -h --help    choose one Calcul from Calcul pharma to perform
+    --quiet      print less text
+    --verbose    print more text
+
+"""
+
+#import docopt
+#from appPharma import calculPharma
+
+
+#args = docopt.docopt(__doc__)
+#print(args) 
+    
+#if arg['calculus']:
+    #print(calculPharma.arg['calculus'])
+        
+print(__doc__)
+    

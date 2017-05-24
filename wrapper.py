f7d237702b956e3a38b79189b40c05d3079d32bb
Hatoris <flotianxbernard@gmail.com>
2017-05-23 20:01:33 -0400

    remove useless module

--- wrapper.py
+++ wrapper.py
@@ -1,30 +1,4 @@
-#wrraper function
 from appPharma import pkg
-
-
-def changeUnit(*units):
-    "use it above function @changeUnit(u1, ux) to change unit as wich" 
-    def units_decorator(func):
-        def func_wraper(*unit):
-            values = []
-            for i in range(len(unit)):
-                if units[i] != 'ignore':
-                    values.append(Q_(unit[i]).to(units[i]))
-                else:
-                    values.append(unit[i])
-            return func(*values)        
-        return func_wraper
-    return units_decorator
-
-def sameUnits():
-    def units_decorator(func):
-        def func_wraper(*unit):
-            values = []
-            for i in range(len(unit)):
-                values.append(Q_(unit[i]).to(units[i]))
-            return func(*values)        
-        return func_wraper
-    return units_decorator
 
 def changeUnits(*units, **kunits):
     """use it above function @changeUnits('kg', 'umol/l', d = 'unityouwant', z = 'ignore' ) to change unit as wich"""
@@ -33,8 +7,6 @@ def changeUnits(*units, **kunits):
         def func_wraper(*unit, **kunit):
             values = [] 
             kvalues = {}
-            #k = list(kunit.keys())
-            #k.sort() 
             for i in range(len(unit)):
                 if units[i] != 'ignore':
                     values.append(pkg.Q_(unit[i]).to(units[i]))

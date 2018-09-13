9c848a7adcf265b00c343c15d37b4fe4bc70e37e
Florian Bernard <florianxbernard@gmail.com>
2018-09-13 10:19:33 -0400

    add mEq definition

--- appPharma/pkg.py
+++ appPharma/pkg.py
@@ -10,5 +10,7 @@ ureg.default_format = "P"
 ureg.define('bmi = kg/m**2')
 #define percent, pct
 ureg.define(UnitDefinition('percent', 'pct',  (), ScaleConverter(1 / 100.0)))
+#define mEq
+ureg.define(UnitDefinition('equivalent', 'Eq',  (), ScaleConverter(1), reference="Quantity"))
 #define ounce liquide
 ureg.define('ozl = 29.5735293017338 * milliliter = ounce_liquid') 
